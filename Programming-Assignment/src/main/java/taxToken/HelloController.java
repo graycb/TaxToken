bea805e279b02fd6a6c7016eee0751f5f78dff54
graycb <34928796+graycb@users.noreply.github.com>
2018-01-02 20:53:38 -0500

    Update HelloController.java

--- Programming-Assignment/src/main/java/taxToken/HelloController.java
+++ Programming-Assignment/src/main/java/taxToken/HelloController.java
@@ -1,1 +1,34 @@
+package taxToken;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	
+	@RequestMapping("/")
+	public String sayHi() {
+		
+		// Returns a HTML page that I created
+		return "<!DOCTYPE html>\n" + 
+				"<html>\n" + 
+				"    \n" + 
+				"    <head>\n" + 
+				"        <meta charset=\"utf-8\">\n" + 
+				"    </head>\n" + 
+				"    <!--Christian Gray-->\n" + 
+				"    <body>\n" + 
+				"        <h1>\n" + 
+				"            <center>Hello World!!!!</center>\n" + 
+				"        </h1>\n" + 
+				"        \n" + 
+				"        \n" + 
+				"        \n" + 
+				"    </body>\n" + 
+				"</html>";
+	} 
+	
+}
 
