b092136a2d6d1c5272da6a20d3a57d74ba278376
graycb <34928796+graycb@users.noreply.github.com>
2018-01-05 18:01:02 -0500

    Update UserService.java

--- Programming-Assignment/src/main/java/taxToken/UserService.java
+++ Programming-Assignment/src/main/java/taxToken/UserService.java
@@ -1,74 +1,73 @@
-package taxToken;
+package TaxToken;
 
-import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 	
-	public static HashMap<String, User> userMap = new HashMap<>();
+	@Autowired
+	private UserRepository userRepository;
 	
 	public UserService() {
 		
 	}
 	
 	// Returns the birthday matching the name passed in
-	public static String returnBirthday(String name, String birthday) {
+	public String returnBirthday(String name, String birthday) {
 		
-		for(String nm : userMap.keySet()) {
-			if(nm.equals(name)) {
-				birthday = userMap.get(name).getBirthday();
-			}
+		if(userRepository.exists(name)) {
+			birthday = userRepository.findOne(name).getBirthday();
 		}
+		
 		return birthday;	
 		
 	}
 	
 	// Returns the age matching the name passed ins
-	public static int returnAge(String name, int age) {
+	public int returnAge(String name, int age) {
 		
-		for(String nm : userMap.keySet()) {
-			if(nm.equals(name)) {
-				age = userMap.get(name).getAge();
-			}
+		if(userRepository.exists(name)) {
+			age = userRepository.findOne(name).getAge();
 		}
 		
 		return age;
 	}
 	
 	// Adds the user passed into the user HashMap
-	public static Boolean addNewUser(User user) {
+	public Boolean addNewUser(User user) {
+		
+		
 		Boolean check = false;
 		
 		if(user.getName() != null && user.getBirthday() != null && user.getAge() != 0) {
 
-			for(String nm : userMap.keySet()) {
-				if(nm.equals(user.getName())) {
-					return check;
-				}
+			if(!userRepository.exists(user.getName())) {
+				
+				userRepository.save(user);
+				check = true;
 			}
-			userMap.put(user.getName(), user);
-			check = true;
-		}
-		
-		
+			
+		} 
+
 		return check;
 		
 	}
 	
 	// Updated the user in the array list that has a name matching what was passed in
-	public static Boolean updateNewUser(String name, User user) {
+	public Boolean updateNewUser(String name, User user) {
 		Boolean check = false;
-		
-		for(String nm : userMap.keySet()) {
-			if(nm.equals(name)) {
-				userMap.replace(name, user);
-				userMap.get(name).setName(name);
-				check = true;
-			}
+		if(!userRepository.exists(name)) {
+			return check;
 		}
 		
-		return check;
-		
-		
+		if(user.getBirthday() != null && user.getAge() != 0) {
+			user.setName(name);
+			userRepository.save(user);
+		}
+		return check;	
 		
 	}
+	
+	
 }
